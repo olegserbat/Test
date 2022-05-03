d48d745a0027a3416992d34a2412fbfd3f97446b
Oleg Iushkevich <olegserbat@yandex.ru>
2022-05-03 12:51:28 +0300

    init

---
+++ 2.php
@@ -0,0 +1,16 @@
+<?php
+$a = readline();
+$b = readline();
+while ($a <= $b) {
+    if ($a % 2 == 0) {
+        echo "$a ";
+    }
+    $a++;
+}
+
+
+//for ($i = $a; $i <= $b; $i++) {
+//    if ($i % 2 == 0) {
+//        echo "$i ";
+//    }
+//}

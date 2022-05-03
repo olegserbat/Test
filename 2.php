47fc027e4da9f39af2b83b8f3542b5429b80272b
Oleg Iushkevich <olegserbat@yandex.ru>
2022-05-03 12:56:45 +0300

    переделал программу

--- 2.php
+++ 2.php
@@ -1,16 +1,10 @@
 <?php
 $a = readline();
 $b = readline();
-while ($a <= $b) {
-    if ($a % 2 == 0) {
-        echo "$a ";
+for ($i = $a; $i <= $b; $i++) {
+    if ($i % 2 == 0) {
+        echo "$i ";
     }
-    $a++;
 }
 
 
-//for ($i = $a; $i <= $b; $i++) {
-//    if ($i % 2 == 0) {
-//        echo "$i ";
-//    }
-//}

f01d477205d79402aba158e8f64ea0d94313bba2
Oleg Iushkevich <olegserbat@yandex.ru>
2022-05-06 10:33:11 +0300

    add the world

--- 2.php
+++ 2.php
@@ -3,7 +3,7 @@ $a = readline();
 $b = readline();
 for ($i = $a; $i <= $b; $i++) {
     if ($i % 2 == 0) {
-        echo "$i ";
+        echo "'четные', $i ";
     }
 }
 
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+define("A",46);
+echo A;
+

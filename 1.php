9f9cc4199305725ba24219ebe17564b5341547d5
Oleg Iushkevich <olegserbat@yandex.ru>
2022-05-29 10:43:06 +0300

    Работа array_reduce

--- 1.php
+++ 1.php
@@ -1,8 +1,17 @@
 <?php
+function sum($carry, $item)
+{
+    $carry += $item;
+    return $carry;
+}
 
-echo <<<TEXT    
-              Поверка
-                  вывода 
-                       текста
-             TEXT;
+/*function product($carry, $item)
+{
+    $carry *= $item;
+    return $carry;
+}
+*/
+$a = array(1, 2, 3, 4, 5);
+$x = array();
 
+var_dump(array_reduce($a, "sum",));

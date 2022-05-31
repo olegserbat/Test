54ffe8832cf8d6dbd03d0f31bf39a52cb533f915
Oleg Iushkevich <olegserbat@yandex.ru>
2022-05-31 14:29:07 +0300

    Решение задачи с анаграммами

--- 2.php
+++ 2.php
@@ -1,10 +1,14 @@
 <?php
-$a = readline();
-$b = readline();
-for ($i = $a; $i <= $b; $i++) {
-    if ($i % 2 == 0) {
-        echo "'четные', $i ";
-    }
+$str1='mama mila';
+$str2='ilma mama';
+$str1_array=str_split($str1);
+$str2_array=str_split($str2);
+$count_str1=(count($str1_array));
+$count_str2=(count($str2_array));
+$compare_str=array_diff($str1_array, $str2_array);
+If ($count_str1==$count_str2 && count ($compare_str)==0) {
+    echo 'строки являются анаграммами';
+} else {
+    echo 'строки не являются анаграммами';
 }
-
 

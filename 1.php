b9ce80d0534755430fbecd171e197e3c7b878351
Oleg Iushkevich <olegserbat@yandex.ru>
2022-06-08 15:38:48 +0300

    Решение задачи с анаграммами дубль 2

--- 1.php
+++ 1.php
@@ -11,7 +11,12 @@ function sum($carry, $item)
     return $carry;
 }
 */
-$a = array(1, 2, 3, 4, 5);
-$x = array();
+$a = array(1, 2, 3, 4, 5, 8, 10);
+//$x = array();
+$result = array_reduce($a, function ($subSum, $val) {
+    echo ("currenct subsum is $subSum; current array value is $val \n");
+    return $subSum += $val;
+});
 
-var_dump(array_reduce($a, "sum",));
+var_dump($result);
+//var_dump(array_reduce($a, "sum",));
--- test.php
+++ test.php
@@ -1,10 +1,20 @@
 <?php
-$a=readline ();
-$b=readline ();
-function sum ($a,$b) {
-    $c=$a+$b;
-    return $c;
-}
-print sum;
-
-
+$str1='mama';
+$str2='amam';
+$anagram=function ($str1,$str2) {
+    $str1_array = str_split($str1);
+    sort($str1_array);
+    $str2_array = str_split($str2);
+    sort($str2_array);
+    $count_str1 = (count($str1_array));
+    $count_str2 = (count($str2_array));
+//var_dump(array_count_values($str1_array));
+    $sum=array_diff_assoc($str2_array, $str1_array);
+//var_dump($str2_array);
+    If ($count_str1=$count_str2 && $sum=0) {
+        echo 'маннограм';
+    } else {
+        echo 'не манограм';
+    }
+};
+print $anagram ($str1,$str2);

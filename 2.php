1682e1bef292e65068fdac4bae796b64fd696b8d
Oleg Iushkevich <olegserbat@yandex.ru>
2022-06-08 15:38:21 +0300

    Решение задачи с анаграммами дубль 2

--- 2.php
+++ 2.php
@@ -1,14 +1,18 @@
 <?php
-$str1='mama mila';
-$str2='ilma mama';
-$str1_array=str_split($str1);
-$str2_array=str_split($str2);
-$count_str1=(count($str1_array));
-$count_str2=(count($str2_array));
-$compare_str=array_diff($str1_array, $str2_array);
-If ($count_str1==$count_str2 && count ($compare_str)==0) {
-    echo 'строки являются анаграммами';
-} else {
-    echo 'строки не являются анаграммами';
-}
-
+$str1='mama mila ramy';
+$str2='ramy alim amamd';
+function anagramm ($str1,$str2)
+{
+    $str1_array = str_split($str1);
+    $str2_array = str_split($str2);
+    sort($str1_array);
+    sort($str2_array);
+    $dif = array_diff_assoc($str1_array, $str2_array);
+    $count_str1 = (count($str1_array));
+    $count_str2 = (count($str2_array));
+    if ($count_str1 == $count_str2 && count($dif) == 0) {
+        echo 'строки являются анаграммами';
+    } else {
+        echo 'строки не являются анаграммами';
+    }
+} print anagramm($str1,$str2);

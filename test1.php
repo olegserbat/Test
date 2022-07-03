26eb68b5a61e5f3f7eaf874b3c3ab5dcc7ea416c
Oleg Iushkevich <olegserbat@yandex.ru>
2022-07-03 15:11:46 +0300

    Решение задачи с красивыми числами c чтением из файла

--- test1.php
+++ test1.php
@@ -1,5 +1,16 @@
 <?php
-$input = [6,7,8,9];
+function getInputData(): array
+{
+    $file = fopen('result.txt', 'r');
+    $countNumb = (int)fgets($file);
+    $input = [];
+    for ($i = 0; $i < $countNumb; $i++) {
+        $input[] = ((int)fgets($file));
+    }
+    fclose($file);
+    return $input;
+}
+
 function isBeautifulNumber(array $dig): bool
 {
     for ($i = 1; $i < count($dig); $i += 2) {
@@ -16,6 +27,7 @@ function isBeautifulNumber(array $dig): bool
 }
 
 $countNiceDigit = 0;
+$input = getInputData();
 for ($k = 0; $k < count($input); $k++) {
     $element_input = str_split(strrev((string)$input[$k]));
     if (isBeautifulNumber($element_input)) {

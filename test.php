102251f6f53179c171eb4e11a2f4f421fe1b35af
Oleg Iushkevich <olegserbat@yandex.ru>
2022-07-29 14:01:03 +0300

    Count the number of Duplicates

--- test.php
+++ test.php
@@ -1,29 +1,13 @@
 <?php
-function solution(string $s): int
-{
-    (int)$array = str_split($s);
-    $arrayReal = str_split($s);
-    (int)arsort($array);
-    $arrayBigKeys = array_keys($array, current($array));
-    $resultArray = [];
-    $a = [];
-    $result = 0;
-    for ($j = 0; $j < count($arrayBigKeys); $j++) {
-        $i = 0;
-        while ($i < 5) {
-            if (($arrayBigKeys[$j] + $i) < count($arrayReal)) {
-                $resultArray[$j][$i] = $arrayReal[$arrayBigKeys[$j] + $i];
-                $i++;
-            } else {
-                break;
-            }
+function duplicateCount($text) {
+        $result=0;
+    $text=strtolower($text);
+    $text=str_split($text);
+    $text=array_count_values($text);
+    foreach ($text as $value){
+        if ($value>1){
+            $result++;
         }
-        if (implode($resultArray[$j]) > $result) {
-            $result = implode ($resultArray[$j]);
-            $a = $resultArray[$j];
-        };
-    };
-    (int)$res = implode($a);
-    return $res;
+    }
+    return $result;
 }
-var_dump( solution("999109999"));
